4049e6818c5a8c30b9f3da736a7922f8be1753bb
Mohammadreza324 <70264596+Mohammadreza324@users.noreply.github.com>
2020-09-02 11:59:43 +0200

    cv

---
+++ introkod-syntax/Syntax.py
@@ -0,0 +1,8 @@
+print("hej klass 19c")
+name = "reza"
+print("Ditt namn är",name )
+
+print ("vi skapar en variabel x och tilldelar det värdet till 5")
+x = 5 
+print("vi skapar yttrerligare en variabel och tilldelar värdet 8")
+print("med kommaoperator: x+y= ", x+y)
